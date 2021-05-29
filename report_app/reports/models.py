7a8db1a0d9d97da9238ce7a690290a370c0e66d8
ronel <ronel.zapata@ovsgroup.com>
2021-05-28 20:33:38 -0500

    added reports model

--- report_app/reports/models.py
+++ report_app/reports/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from profiles.models import Profile
+
 
 # Create your models here.
+class Report(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to="reports", blank=True)
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+    
